b542055efc43e10d413e18d9c0628b503f23625d
Nicole Nouwels <nicole.n.nouwels@gmail.com>
2019-02-14 20:40:44 -0800

    assignment 1

---
+++ JS/script.js
@@ -0,0 +1,157 @@
+/*eslint-env browser*/
+
+/*STEP 1
+var someMonth;
+function theMonth();
+var currentMonth;
+var summerMonth;
+var MyLibraryFunction
+*/
+
+/* STEP 2
+4.5
+"Nicole Nouwels"
+true
+null
+*/
+
+/*STEP 3
+var speed = (100/4) * 2;
+var distance =  (1000/5) * 2;
+var travel = speed + distance;*/
+
+/*
+STEP 4
+var firstName; 
+var lastName; 
+var Address; 
+var City; 
+var State; 
+var zipCode; 
+var yourAge; 
+var referralSource; 
+var mayWeContactYou;
+*/
+
+/*
+STEP 5
+var firstName = "Nicole."
+var lastName = "Nouwels." 
+var Address = "123 Some Street" 
+var City; 
+var State; 
+var zipCode; 
+var yourAge; 
+var referralSource; 
+var mayWeContactYou;
+*/
+
+/*STEP 6
+var x = 5;
+var y = x + "String";
+window.console.log(y);*/
+
+/*
+STEP 7
+var n = "car is red" + true;
+window.console.log(n);
+
+var p = 10 + false;
+window.console.log(p);*/
+
+/*STEP 8
+var someString = "Who once said," + "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.";
+window.console.log(someString);*/
+
+/*STEP 9
+var z;
+window.console.log(z);
+
+var u = null;
+window.console.log(u);*/
+
+/*STEP 10
+console.log (typeof 13);
+console.log (typeof "the sky is blue");
+console.log (typeof true);*/
+
+/*STEP 11
+alert("Hello Zak Ruvalcaba, " + "Welcome to the JavaScript class!")*/
+
+/*STEP 13
+var name = "Nicole Nouwels";
+alert(name + " Welcome to the JavaScript class!");*/
+
+/*
+STEP 14
+var name = "Nicole Nouwels";
+var course = "JavaScript";
+alert(name + "," + " Welcome to the " + course + " class!");
+
+alert(name + ",\n" + "Welcome to the " + course + " class!");
+*/
+
+/*STEP 15 & 16
+var name = window.prompt("What is your name?");
+var course = window.prompt("What class are you taking?");*/
+
+/*
+STEP 17
+var x = 10;
+var y = 20;
+window.console.log(x + y);
+*/
+
+/*STEP 18
+var x = 20;
+window.console.log(x + 20);*/
+
+/*STEP 19
+var y = 20;
+console.log(y * 5);*/
+
+/*STEP 20
+var p = 20;
+console.log(p / 3);*/
+
+/*STEP 21
+var a = 15;
+var b = 20;
+console.log(a <= b);*/
+
+/*STEP 22
+var c = 200;
+var d = 250;
+
+console.log(c == d || c > d);*/
+
+/*STEP 23
+var widget = new Object();
+    widget.type = "calendar";
+
+
+console.log(typeof widget);*/
+
+
+/*
+STEP 24
+var widget = new Object();
+    widget.type = "calendar";
+
+
+console.log(widget instanceof Object);
+*/
+
+/*
+step 25
+var widget = new Object();
+    widget.type = "calendar";
+
+
+console.log(!(widget instanceof Object));
+*/
+
+
+
+
+
